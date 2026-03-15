c587ed133b4f064c07083df1b5aa530146cb3e04
YuHsuanChen <ddy20824@gmail.com>
2026-03-15 02:34:55 -0400

    Add GameConfig, Events  and Ingredient's lifebar and capacity

---
+++ Assets/Scripts/Enum/IngredientType.cs
@@ -0,0 +1,5 @@
+public enum IngredientType
+{
+    Tomato,
+    Pumpkin,
+}
---
+++ Assets/Scripts/SO/Event/IngredientTypeEventChannel.cs
@@ -0,0 +1,4 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Events/IngredientType Event Channel")]
+public class IngredientTypeEventChannel : BaseEventChannel<IngredientType> { }
