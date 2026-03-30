72e5c1b27a8ffc4b5f3d22c2d92eedc882fcb14f
Minh Khoi Phan <pmka42@gmail.com>
2026-03-30 09:53:51 -0400

    Added star mechanics and advice based on score.

--- Assets/Scripts/UI/Gameplay/ResultBoard.cs
+++ Assets/Scripts/UI/Gameplay/ResultBoard.cs
@@ -8,13 +8,20 @@ using UnityEngine.UI;
 
 public class ResultBoard : BasePanel<ResultBoard>
 {
+    [SerializeField] private PauseMenu pauseMenuScript;
+    [SerializeField] private Button btnSettings;
+
     [SerializeField] private Button btnNextLevel;
     [SerializeField] private Button btnBackToMain;
     [SerializeField] private TextMeshProUGUI successfulDish;
     [SerializeField] private TextMeshProUGUI failedDish;
     [SerializeField] private TextMeshProUGUI rottenIngr;
     [SerializeField] private TextMeshProUGUI totalScore;
+    [SerializeField] private TextMeshProUGUI freeAdvice;
     [SerializeField] private GameObject blurVolume;
+    [SerializeField] private Sprite[] starStates;
+    [SerializeField] private Image[] stars;
+    [SerializeField] private int testScore = 10;
 
     [Header("Events")]
     [SerializeField] private SFXTypeEventChannel onSFXRequest;
@@ -22,6 +29,7 @@ public class ResultBoard : BasePanel<ResultBoard>
     {
         base.Awake();
         blurVolume.SetActive(false);
+        InitStarImages();
         HideMe();
     }
 
@@ -47,6 +55,7 @@ public class ResultBoard : BasePanel<ResultBoard>
             onSFXRequest.Raise(GameplaySFXType.LEVEL_COMPLETE);
             DisableAllGameInteractions();
             blurVolume.SetActive(true);
+            SetLevelSummary(testScore);
             ShowMe();
         }
     }
@@ -62,6 +71,8 @@ public class ResultBoard : BasePanel<ResultBoard>
         Time.timeScale = 1.0f;
         SoundManager.Instance.ToggleGameMusic(true);
         blurVolume.SetActive(false);
+        pauseMenuScript.enabled = true;
+        btnSettings.enabled = true;
         HideMe();
     }
 
@@ -97,8 +108,61 @@ public class ResultBoard : BasePanel<ResultBoard>
         {
             script.enabled = false;
         }
+
+        // Disable the pause menu as well
+        pauseMenuScript.enabled = false;
+        btnSettings.enabled = false;
     }
 
+    private void InitStarImages()
+    {
+        foreach (Image img in stars)
+        {
+            img.sprite = starStates[0];
+        }
+    }
+
+    private void SetLevelSummary(int score)
+    {
+        SetStars(score);
+        SetAdvice(score);
+    }
+
+    private void SetStars(int score)
+    {
+        if (score >= 500)
+        {
+            stars[2].sprite = starStates[1];
+        }
+        if (score >= 200)
+        {
+            stars[1].sprite = starStates[1];           
+        }
+        if (score >= 100)
+        {
+            stars[0].sprite = starStates[1];
+        }
+    }
+
+    private void SetAdvice(int score)
+    {
+        if (score >= 500)
+        {
+            freeAdvice.text = "You did a great job!";
+        }
+        else if (score >= 200)
+        {
+            freeAdvice.text = "Good job! Pay attention to the expiring resources in the box to prevent rotten ingredients.";
+        }
+        else if (score >= 100)
+        {
+            freeAdvice.text = "Well done! Don't waste too much food mkay!";
+        }
+        else
+        {
+            freeAdvice.text = "Don't worry! An environmentalist will contact you shortly.";
+        }
+    }
     private void TestUI() // Only for testing
     {
         successfulDish.text = $"3 x 100 = 300";
